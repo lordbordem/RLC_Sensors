33981f9e7f35f0d4c55bfba5782548df15116489
lordbordem <lordbordem@gmail.com>
2017-03-16 18:14:33 +1100

    Got People Counter Working
    People Counter Works and added to DB (OUT)

--- server/app.js
+++ server/app.js
@@ -16,38 +16,51 @@ var async = require('async');
 
 var locationList = ['']
 
+var peopleCounterLocationList = ['PC00.07', 'PC01.12', 'PC01.13', 'PC02.14', 'PC02.16', 'PC05.22',  'PC09.27', 'PC09.28', 'PC11.33'];
 
-var peopleCounterLocationList = ['PCB101-LevelB1CenterPCLabs1', 'PCB102-LevelB1WesternPCLabs1', 'PC0005-WattleStreetWest1', 'PC0006-WattleStreetWest2', 'PC0007-WattleStB10Entrance', 'PC0008-BroadwayWest2Stairs', 'PC0009-BroadwayWest1', 'PC0111-CafeWest', 'PC0112-StairsToLevel2', 'PC0113-PennyLaneExit', 'PC0214-BroadwayEastEntrance', 'PC0215-DataArena', 'PC0216-JonesStEntrance', 'PC0420-Classroom400', 'PC0522-Level5StairsDown', 'PC0523-Classroom200', 'PC0825-CHTArea', 'PC0926-CassArea', 'PC0927-Level9StairsUp', 'PC0928-Level9StairsDown', 'PC0929-StaffCommonRoom', 'PC1030-Level10BridgeEast', 'PC1131-Level11BridgeEast', 'PC1132-SchoolOfCivil', 'PC1133-Level11StairsUp'];
+var dbPeopleCounterOUT =
+  {
+    "PC00.07": {"OUT": 21},
+    "PC01.12": {"OUT": 21},
+    "PC01.13": {"OUT": 21},
+    "PC02.14": {"OUT": 21},
+    "PC02.16": {"OUT": 21},
+    "PC05.22": {"OUT": 21},
+    "PC09.27": {"OUT": 21},
+    "PC09.28": {"OUT": 21},
+    "PC11.33": {"OUT": 21}
+  };
+  
+var dbPeopleCounterIN =
+  {
+    "PC00.07": {"IN": 21},
+    "PC01.12": {"IN": 21},
+    "PC01.13": {"IN": 21},
+    "PC02.14": {"IN": 21},
+    "PC02.16": {"IN": 21},
+    "PC05.22": {"IN": 21},
+    "PC09.27": {"IN": 21},
+    "PC09.28": {"IN": 21}
+  };
+
+
+
+//var peopleCounterLocationList = ['PC0007-WattleStB10Entrance', 'PC0112-StairsToLevel2', 'PC0113-PennyLaneExit', 'PC0214-BroadwayEastEntrance', 'PC0216-JonesStEntrance', 'PC0522-Level5StairsDown',  'PC0927-Level9StairsUp', 'PC0928-Level9StairsDown', 'PC1133-Level11StairsUp'];
 /*
 var dbPeopleCounterList =
   {
-    "PCB101-LevelB1CenterPCLabs1": {"Out": },
-    "PCB102-LevelB1WesternPCLabs1": {},
-    "PC0005-WattleStreetWest1": {},
-    "PC0006-WattleStreetWest2": {},
     "PC0007-WattleStB10Entrance": {},
-    "PC0008-BroadwayWest2Stairs": {},
-    "PC0009-BroadwayWest1": {},
-    "PC0111-CafeWest": {},
     "PC0112-StairsToLevel2": {},
     "PC0113-PennyLaneExit": {},
     "PC0214-BroadwayEastEntrance": {},
-    "PC0215-DataArena": {},
     "PC0216-JonesStEntrance": {},
-    "PC0420-Classroom400": {},
     "PC0522-Level5StairsDown": {},
-    "PC0523-Classroom200": {},
-    "PC0825-CHTArea": {},
-    "PC0926-CassArea": {},
     "PC0927-Level9StairsUp": {},
     "PC0928-Level9StairsDown": {},
-    "PC0929-StaffCommonRoom": {},
-    "PC1030-Level10BridgeEast": {},
-    "PC1131-Level11BridgeEast": {},
-    "PC1132-SchoolOfCivil": {},
     "PC1133-Level11StairsUp": {}
   };
 */
+
 var underscore = require("underscore.string");
 var math = require('mathjs');
 
@@ -55,7 +68,8 @@ var sqlite3 = require('sqlite3').verbose()
 ,   db = new sqlite3.Database('RCLSequelize.db')
 ,   Sequelize = require('sequelize');;
 
-var waspmoteList = ['ES_B_04_415_7BD1','ES_B_05_416_7C15','ES_B_10_427_7B90','ES_B_07_420_7E1D','ES_B_09_425_3E8D','ES_B_10_426_7E1E','ES_B_05_417_7C13','ES_B_01_411_7E39','ES_A_05_175_CE2E','ES_B_09_424_B74C','ES_B_12_431_7BC2','ES_B_04_414_7C00','ES_B_07_421_7C17','ES_B_11_429_3E90','ES_B_08_423_7BE2','ES_B_12_430_44DE','ES_B_02_412_3E68','ES_B_06_418_7BED','ES_C_13_302_C88E','ES_B_06_419_7C09','ES_B_08_422_7BDC','ES_B_11_428_3EA4','ES_A_03_141_CE3A','ES_A_03_151_CE38','ES_A_07_293_3E72','ES_A_03_153_CE2A','ES_A_04_163_7C58','ES_A_06_195_3E87','ES_A_13_276_7C44','ES_A_04_159_7B7E','ES_A_09_298_7BC5','ES_A_12_301_7BB6','ES_A_09_226_44DF','ES_A_07_207_44FA','ES_A_06_291_CE30','ES_A_05_181_7BDA','ES_A_11_245_CE2B','ES_A_11_300_7BCD','ES_A_10_234_3E9E','ES_A_06_292_7BBF','ES_A_07_294_7BBC']
+//var waspmoteList = ['ES_B_04_415_7BD1','ES_B_05_416_7C15','ES_B_10_427_7B90','ES_B_07_420_7E1D','ES_B_09_425_3E8D','ES_B_10_426_7E1E','ES_B_05_417_7C13','ES_B_01_411_7E39','ES_A_05_175_CE2E','ES_B_09_424_B74C','ES_B_12_431_7BC2','ES_B_04_414_7C00','ES_B_07_421_7C17','ES_B_11_429_3E90','ES_B_08_423_7BE2','ES_B_12_430_44DE','ES_B_02_412_3E68','ES_B_06_418_7BED','ES_C_13_302_C88E','ES_B_06_419_7C09','ES_B_08_422_7BDC','ES_B_11_428_3EA4','ES_A_03_141_CE3A','ES_A_03_151_CE38','ES_A_07_293_3E72','ES_A_03_153_CE2A','ES_A_04_163_7C58','ES_A_06_195_3E87','ES_A_13_276_7C44','ES_A_04_159_7B7E','ES_A_09_298_7BC5','ES_A_12_301_7BB6','ES_A_09_226_44DF','ES_A_07_207_44FA']
+/*
 var dbWaspmote = {
 	"ES_B_04_415_7BD1": {"CO2": 21},
 	"ES_B_05_416_7C15": {"CO2": 21},
@@ -90,16 +104,9 @@ var dbWaspmote = {
 	"ES_A_09_298_7BC5": {"CO2": 21},
 	"ES_A_12_301_7BB6": {"CO2": 21},
 	"ES_A_09_226_44DF": {"CO2": 21},
-	"ES_A_07_207_44FA": {"CO2": 21},
-	"ES_A_06_291_CE30": {"CO2": 21},
-	"ES_A_05_181_7BDA": {"CO2": 21},
-	"ES_A_11_245_CE2B": {"CO2": 21},
-	"ES_A_11_300_7BCD": {"CO2": 21},
-	"ES_A_10_234_3E9E": {"CO2": 21},
-	"ES_A_06_292_7BBF": {"CO2": 21},
-	"ES_A_07_294_7BBC": {"CO2": 21}	
+	"ES_A_07_207_44FA": {"CO2": 21}
 };
-
+*/
 
 var roomsList = ['E1-04201', 'E1-05204', 'E1-05300', 'E1-053L0', 'E1-053V0', 'E1-05402', 'E1-06101', 'E1-06102', 'E1-06103', 'E1-061V2', 'E1-07403', 'E1-07404', 'E1-07405', 'E1-07406', 'E1-08409','E1-09405', 'E1-10104', 'E1-11300', 'E1-11302', 'E1-11403', 'E1-B1100', 'E1-B1102', 'E1-B1103', 'E1-B11V2', 'E1-B11V3', 'E1-B1203', 'E1-B1204', 'E1-B1400', 'E1-B1401', 'E1-B1402', 'E1-B1403', 'E1-B14V0', 'E1-B14V1', 'E1-B14V2', 'E1-B14V3'];
 var dbRooms =
@@ -142,8 +149,74 @@ var dbRooms =
 };
 
 var dbLevels = {};
+var counter = 0;
 
 
+var getPeopleCounter = function(pplCounterList, pplCount_list){
+	//Get time now, and time half an hour ago, and transform it into the correct format
+	var toDate = getTimeNow();
+	var fromDate = getTime48HourAgo();
+
+
+	// Debugging time formats
+	// console.log(toDate);
+	// console.log(fromDate);
+
+
+	//For every waspmote in the predefined array, retrieve the json from the specfic url
+	for(let pplCount of pplCount_list){
+
+		var request = require('request');
+		var url = "https://eif-research.feit.uts.edu.au/api/json/?rFromDate="+ fromDate +"&rToDate="+ toDate +"&rFamily=people&rSensor=" + "+" + pplCount + "+" + "%28Out%29"
+	
+		console.log("the url is.." + url);
+		counter++;
+		
+		//console.log(waspmote);
+		//Line added to deal with the authorisation request from the uts api. 
+		//(note: it is not the best practice and not very secure, but till an alternative is found, this is the solution)
+		process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+		console.log("The count of waspmote currently is... " + counter);
+		//Debugging
+		console.log("Sending request");
+		request(url , function (error, response, body) {
+			//Debugging
+			console.log("got response");
+			console.log(body);
+			//Alternative fix to the authorisation problem, still in progress (slows down everything)
+			//require('ssl-root-cas').inject();
+
+			if (!error && response.statusCode == 200) {
+			 	//Gets the body response from the request call and formats it to json
+			 	//Adds information to dbRooms
+				addInfoTodbPPlCount(body, pplCount);
+	 
+			}else{
+			  	console.log(error);
+
+			}
+		})
+	}
+}
+
+
+
+//Adds information to dbRooms, and also calls populate levels
+//The function"Populate levels" is being called in here to
+//avoid js asynchronus execution
+var addInfoTodbPPlCount = function(body, pplCount){
+	console.log("The current pplCount before is... " + counter);
+	var jsonBody = JSON.parse(body);
+	for(var attributename in jsonBody){
+		console.log("The current waspmote is... " + attributename);
+    	dbPeopleCounterOUT[pplCount]["OUT"] = jsonBody[attributename][1];
+		console.log(dbPeopleCounterOUT[pplCount]["OUT"]);
+	}
+
+}
+
+
+/*
 var getWaspmotes = function(waspmoteList, waspmote_list){
 	//Get time now, and time half an hour ago, and transform it into the correct format
 	var toDate = getTimeNow();
@@ -161,12 +234,12 @@ var getWaspmotes = function(waspmoteList, waspmote_list){
 		var request = require('request');
 		var url = "https://eif-research.feit.uts.edu.au/api/json/?rFromDate="+ fromDate +"&rToDate="+ toDate +"&rFamily=wasp&rSensor=" + waspmote + "&rSubSensor=CO2"
 		console.log("the url is.." + url);
-
+		counter++;
 		//console.log(waspmote);
 		//Line added to deal with the authorisation request from the uts api. 
 		//(note: it is not the best practice and not very secure, but till an alternative is found, this is the solution)
 		process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
+		console.log("The count of waspmote currently is... " + counter);
 		//Debugging
 		console.log("Sending request");
 		request(url , function (error, response, body) {
@@ -195,9 +268,10 @@ var getWaspmotes = function(waspmoteList, waspmote_list){
 //The function"Populate levels" is being called in here to
 //avoid js asynchronus execution
 var addInfoTodbWaspmote = function(body, waspmote){
+	console.log("The current waspmote before is... " + counter);
 	var jsonBody = JSON.parse(body);
 	for(var attributename in jsonBody){
-
+		console.log("The current waspmote is... " + attributename);
     	dbWaspmote[waspmote]["CO2"] = jsonBody[attributename][1];
 		console.log(dbWaspmote[waspmote]["CO2"]);
 	}
@@ -205,7 +279,7 @@ var addInfoTodbWaspmote = function(body, waspmote){
 }
 
 
-
+*/
 
 
 var getCurrPcLogin = function(dbRooms, rooms_list){
@@ -346,9 +420,33 @@ var getTimeHalfHourAgo = function(){
 
 }
 
+var getTime24HourAgo = function(){
+	var now = moment().format();
+	var halfHourAgo = moment(now).subtract("24", "hours").format();
+	var timenow = halfHourAgo.toString().split("+");
+	var timeSplit = timenow[0].toString().split(":");
+	var finalNow = timeSplit[0] + "%3A" + timeSplit[1] + "%3A" + timeSplit[2];
+
+	return finalNow;
+
+}
+
+var getTime48HourAgo = function(){
+	var now = moment().format();
+	var halfHourAgo = moment(now).subtract("48", "hours").format();
+	var timenow = halfHourAgo.toString().split("+");
+	var timeSplit = timenow[0].toString().split(":");
+	var finalNow = timeSplit[0] + "%3A" + timeSplit[1] + "%3A" + timeSplit[2];
+
+	return finalNow;
+
+}
+
+
 //Calls to function
-getCurrPcLogin(dbRooms, roomsList);
-getWaspmotes(dbWaspmote,waspmoteList);
+//getCurrPcLogin(dbRooms, roomsList);
+//getWaspmotes(dbWaspmote,waspmoteList);
+getPeopleCounter(dbPeopleCounterOUT,peopleCounterLocationList);
 
 var serverIP = ip.address() ;
 setTimeout(function(){ 
