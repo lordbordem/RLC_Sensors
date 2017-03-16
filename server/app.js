576563f285f606bfba2e66b4e08ccdcfbcebacda
lordbordem <lordbordem@gmail.com>
2017-03-16 16:19:26 +1100

    Last Update
    Changed Waspmote Sensor Function (Crashes at add to DB)

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@
 
 var express = require('express')
 var app = express()
+  , server = require('http').createServer(app)
   , io = require('socket.io')(server) // new
   , events = require('events')
   , EventEmitter = require('events').EventEmitter
-  , ejs= require('ejs');;;;
+  , ip = require('ip')
+  , ejs= require('ejs');;;;;;
 
 var path = require('path')
 var moment = require('moment');
@@ -21,49 +23,49 @@ var sqlite3 = require('sqlite3').verbose()
 
 var waspmoteList = ['ES_B_04_415_7BD1','ES_B_05_416_7C15','ES_B_10_427_7B90','ES_B_07_420_7E1D','ES_B_09_425_3E8D','ES_B_10_426_7E1E','ES_B_05_417_7C13','ES_B_01_411_7E39','ES_A_05_175_CE2E','ES_B_09_424_B74C','ES_B_12_431_7BC2','ES_B_04_414_7C00','ES_B_07_421_7C17','ES_B_11_429_3E90','ES_B_08_423_7BE2','ES_B_12_430_44DE','ES_B_02_412_3E68','ES_B_06_418_7BED','ES_C_13_302_C88E','ES_B_06_419_7C09','ES_B_08_422_7BDC','ES_B_11_428_3EA4','ES_A_03_141_CE3A','ES_A_03_151_CE38','ES_A_07_293_3E72','ES_A_03_153_CE2A','ES_A_04_163_7C58','ES_A_06_195_3E87','ES_A_13_276_7C44','ES_A_04_159_7B7E','ES_A_09_298_7BC5','ES_A_12_301_7BB6','ES_A_09_226_44DF','ES_A_07_207_44FA','ES_A_06_291_CE30','ES_A_05_181_7BDA','ES_A_11_245_CE2B','ES_A_11_300_7BCD','ES_A_10_234_3E9E','ES_A_06_292_7BBF','ES_A_07_294_7BBC']
 var dbWaspmote = {
-	"ES_B_04_415_7BD1": {"capacity": 21},
-	"ES_B_05_416_7C15": {"capacity": 21},
-	"ES_B_10_427_7B90": {"capacity": 21},
-	"ES_B_07_420_7E1D": {"capacity": 21},
-	"ES_B_09_425_3E8D": {"capacity": 21},
-	"ES_B_10_426_7E1E": {"capacity": 21},
-	"ES_B_05_417_7C13": {"capacity": 21},
-	"ES_B_01_411_7E39": {"capacity": 21},
-	"ES_A_05_175_CE2E": {"capacity": 21},
-	"ES_B_09_424_B74C": {"capacity": 21},
-	"ES_B_12_431_7BC2": {"capacity": 21},
-	"ES_B_04_414_7C00": {"capacity": 21},
-	"ES_B_07_421_7C17": {"capacity": 21},
-	"ES_B_11_429_3E90": {"capacity": 21},
-	"ES_B_08_423_7BE2": {"capacity": 21},
-	"ES_B_12_430_44DE": {"capacity": 21},
-	"ES_B_02_412_3E68": {"capacity": 21},
-	"ES_B_06_418_7BED": {"capacity": 21},
-	"ES_C_13_302_C88E": {"capacity": 21},
-	"ES_B_06_419_7C09": {"capacity": 21},
-	"ES_B_08_422_7BDC": {"capacity": 21},
-	"ES_B_11_428_3EA4": {"capacity": 21},
-	"ES_A_03_141_CE3A": {"capacity": 21},
-	"ES_A_03_151_CE38": {"capacity": 21},
-	"ES_A_07_293_3E72": {"capacity": 21},
-	"ES_A_03_153_CE2A": {"capacity": 21},
-	"ES_A_04_163_7C58": {"capacity": 21},
-	"ES_A_06_195_3E87": {"capacity": 21},
-	"ES_A_13_276_7C44": {"capacity": 21},
-	"ES_A_04_159_7B7E": {"capacity": 21},
-	"ES_A_09_298_7BC5": {"capacity": 21},
-	"ES_A_12_301_7BB6": {"capacity": 21},
-	"ES_A_09_226_44DF": {"capacity": 21},
-	"ES_A_07_207_44FA": {"capacity": 21},
-	"ES_A_06_291_CE30": {"capacity": 21},
-	"ES_A_05_181_7BDA": {"capacity": 21},
-	"ES_A_11_245_CE2B": {"capacity": 21},
-	"ES_A_11_300_7BCD": {"capacity": 21},
-	"ES_A_10_234_3E9E": {"capacity": 21},
-	"ES_A_06_292_7BBF": {"capacity": 21},
-	"ES_A_07_294_7BBC": {"capacity": 21}
-	
+	"ES_B_04_415_7BD1": {"CO2": 21},
+	"ES_B_05_416_7C15": {"CO2": 21},
+	"ES_B_10_427_7B90": {"CO2": 21},
+	"ES_B_07_420_7E1D": {"CO2": 21},
+	"ES_B_09_425_3E8D": {"CO2": 21},
+	"ES_B_10_426_7E1E": {"CO2": 21},
+	"ES_B_05_417_7C13": {"CO2": 21},
+	"ES_B_01_411_7E39": {"CO2": 21},
+	"ES_A_05_175_CE2E": {"CO2": 21},
+	"ES_B_09_424_B74C": {"CO2": 21},
+	"ES_B_12_431_7BC2": {"CO2": 21},
+	"ES_B_04_414_7C00": {"CO2": 21},
+	"ES_B_07_421_7C17": {"CO2": 21},
+	"ES_B_11_429_3E90": {"CO2": 21},
+	"ES_B_08_423_7BE2": {"CO2": 21},
+	"ES_B_12_430_44DE": {"CO2": 21},
+	"ES_B_02_412_3E68": {"CO2": 21},
+	"ES_B_06_418_7BED": {"CO2": 21},
+	"ES_C_13_302_C88E": {"CO2": 21},
+	"ES_B_06_419_7C09": {"CO2": 21},
+	"ES_B_08_422_7BDC": {"CO2": 21},
+	"ES_B_11_428_3EA4": {"CO2": 21},
+	"ES_A_03_141_CE3A": {"CO2": 21},
+	"ES_A_03_151_CE38": {"CO2": 21},
+	"ES_A_07_293_3E72": {"CO2": 21},
+	"ES_A_03_153_CE2A": {"CO2": 21},
+	"ES_A_04_163_7C58": {"CO2": 21},
+	"ES_A_06_195_3E87": {"CO2": 21},
+	"ES_A_13_276_7C44": {"CO2": 21},
+	"ES_A_04_159_7B7E": {"CO2": 21},
+	"ES_A_09_298_7BC5": {"CO2": 21},
+	"ES_A_12_301_7BB6": {"CO2": 21},
+	"ES_A_09_226_44DF": {"CO2": 21},
+	"ES_A_07_207_44FA": {"CO2": 21},
+	"ES_A_06_291_CE30": {"CO2": 21},
+	"ES_A_05_181_7BDA": {"CO2": 21},
+	"ES_A_11_245_CE2B": {"CO2": 21},
+	"ES_A_11_300_7BCD": {"CO2": 21},
+	"ES_A_10_234_3E9E": {"CO2": 21},
+	"ES_A_06_292_7BBF": {"CO2": 21},
+	"ES_A_07_294_7BBC": {"CO2": 21}	
 };
+
 
 var roomsList = ['E1-04201', 'E1-05204', 'E1-05300', 'E1-053L0', 'E1-053V0', 'E1-05402', 'E1-06101', 'E1-06102', 'E1-06103', 'E1-061V2', 'E1-07403', 'E1-07404', 'E1-07405', 'E1-07406', 'E1-08409','E1-09405', 'E1-10104', 'E1-11300', 'E1-11302', 'E1-11403', 'E1-B1100', 'E1-B1102', 'E1-B1103', 'E1-B11V2', 'E1-B11V3', 'E1-B1203', 'E1-B1204', 'E1-B1400', 'E1-B1401', 'E1-B1402', 'E1-B1403', 'E1-B14V0', 'E1-B14V1', 'E1-B14V2', 'E1-B14V3'];
 var dbRooms = 
@@ -106,6 +108,57 @@ var dbRooms =
 };
 
 var dbLevels = {};
+
+
+var getWaspmotes = function(waspmoteList, waspmote_list){
+	//Get time now, and time half an hour ago, and transform it into the correct format
+	var toDate = getTimeNow();
+	var fromDate = getTimeHalfHourAgo();
+
+
+	// Debugging time formats
+	// console.log(toDate);
+	// console.log(fromDate);
+
+
+	//For every room in the predefined array, retrieve the json from the specfic url
+	for(let waspmote of waspmote_list){
+
+		var request = require('request');
+		var url = "https://eif-research.feit.uts.edu.au/api/json/?rFromDate="+ fromDate +"&rToDate="+ toDate +"&rFamily=wasp&rSensor=" + waspmote + "&rSubSensor=CO2"
+		
+
+		console.log(waspmote);
+		//Line added to deal with the authorisation request from the uts api. 
+		//(note: it is not the best practice and not very secure, but till an alternative is found, this is the solution)
+		process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+		//Debugging
+		console.log("Sending request");
+		console.log(waspmote);
+		request(url , function (error, response, body) {
+			//Debugging
+			console.log("got response");
+
+			//Alternative fix to the authorisation problem, still in progress (slows down everything)
+			//require('ssl-root-cas').inject();
+
+			if (!error && response.statusCode == 200) {
+			 	//Gets the body response from the request call and formats it to json
+			 	//Adds information to dbRooms
+				addInfoTodbRooms(body, room);
+	 
+			}else{
+			  	console.log(error);
+
+			}
+		})
+	}
+}
+
+
+
+
 
 var getCurrPcLogin = function(dbRooms, rooms_list){
 	//Get time now, and time half an hour ago, and transform it into the correct format
@@ -247,6 +300,7 @@ var getTimeHalfHourAgo = function(){
 
 //Calls to function
 getCurrPcLogin(dbRooms, roomsList);
+getWaspmotes(dbWaspmote,waspmoteList);
 
 var serverIP = ip.address() ;
 setTimeout(function(){ 
