2b2b7c3aa1509bc5f987ec0b5c41c419474c1f21
alastairjb <alastairjb@me.com>
2017-03-16 15:30:22 +1100

    Delete some rooms
    Delete some rooms

--- server/app.js
+++ server/app.js
@@ -7,43 +7,13 @@ var path = require('path')
 var moment = require('moment');
 
 var roomsList = ['E1-04201', 'E1-05204', 'E1-05300', 'E1-053L0', 'E1-053V0', 'E1-05402', 'E1-06101', 'E1-06102', 'E1-06103', 'E1-061V2', 'E1-07403', 'E1-07404', 'E1-07405', 'E1-07406', 'E1-08409','E1-09405', 'E1-10104', 'E1-11300', 'E1-11302', 'E1-11403', 'E1-B1100', 'E1-B1102', 'E1-B1103', 'E1-B11V2', 'E1-B11V3', 'E1-B1203', 'E1-B1204', 'E1-B1400', 'E1-B1401', 'E1-B1402', 'E1-B1403', 'E1-B14V0', 'E1-B14V1', 'E1-B14V2', 'E1-B14V3'];
-var dbRooms = 
+var dbRooms =
  {
     "E1-07404": {"capacity": 21},
     "E1-11403": {"capacity": 28},
     "E1-07406": {"capacity": 29},
     "E1-07403": {"capacity": 21},
-    "E1-B1204": {"capacity": 31},
-    "E1-B1400": {"capacity": 36},
-    "E1-B1401": {"capacity": 43},
-    "E1-B1402": {"capacity": 47},
-    "E1-B1403": {"capacity": 47},
-    "E1-B14V2": {"capacity": 47},
-    "E1-B14V3": {"capacity": 46},
-    "E1-061V2": {"capacity": 27},
-    "E1-B14V1": {"capacity": 44},
-    "E1-05402": {"capacity": 4},
-    "E1-10104": {"capacity": 33},
-    "E1-07405": {"capacity": 29},
-    "E1-B14V0": {"capacity": 34},
-    "E1-04201": {"capacity": 7},
-    "E1-B1203": {"capacity": 31},
-    "E1-B1100": {"capacity": 36},
-    "E1-B1103": {"capacity": 31},
-    "E1-B1102": {"capacity": 30},
-    "E1-053L0": {"capacity": 6},
-    "E1-05204": {"capacity": 4},
-    "E1-09405": {"capacity": 51},
-    "E1-08409": {"capacity": 39},
-    "E1-053V0": {"capacity": 10},
-    "E1-06102": {"capacity": 31},
-    "E1-06103": {"capacity": 15},
-    "E1-06101": {"capacity": 25},
-    "E1-B11V3": {"capacity": 29},
-    "E1-B11V2": {"capacity": 27},
-    "E1-11300": {"capacity": 11},
-    "E1-05300": {"capacity": 22},
-    "E1-11302": {"capacity": 15}
+    
 };
 
 var dbLevels = {};
@@ -66,7 +36,7 @@ var getCurrPcLogin = function(dbRooms, rooms_list){
 		var url = "https://eif-research.feit.uts.edu.au/api/json/?rFromDate="+ fromDate +"&rToDate="+ toDate +"&rFamily=logins&rSensor=" + room
 
 		console.log(room);
-		//Line added to deal with the authorisation request from the uts api. 
+		//Line added to deal with the authorisation request from the uts api.
 		//(note: it is not the best practice and not very secure, but till an alternative is found, this is the solution)
 		process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
@@ -83,7 +53,7 @@ var getCurrPcLogin = function(dbRooms, rooms_list){
 			 	//Gets the body response from the request call and formats it to json
 			 	//Adds information to dbRooms
 				addInfoTodbRooms(body, room);
-	 
+
 			}else{
 			  	console.log(error);
 
@@ -94,7 +64,7 @@ var getCurrPcLogin = function(dbRooms, rooms_list){
 
 
 //Adds information to dbRooms, and also calls populate levels
-//The function"Populate levels" is being called in here to 
+//The function"Populate levels" is being called in here to
 //avoid js asynchronus execution
 var addInfoTodbRooms = function(body, room){
 	var jsonBody = JSON.parse(body);
@@ -160,7 +130,7 @@ var populateLevels = function(rooms_list, room){
 
 
 //function turns time into the correct format to put in the url for the api retrieval
-//eg of the correct format 
+//eg of the correct format
 //2017-03-01T09%3A31%3A44
 var getTimeNow = function(){
 	var now = moment().format();
@@ -172,7 +142,7 @@ var getTimeNow = function(){
 
 }
 
-//Similar role to the function above, but instead of returning the time now, 
+//Similar role to the function above, but instead of returning the time now,
 //returns the time 30 minutes ago from now, in the correct format
 //for url
 var getTimeHalfHourAgo = function(){
@@ -202,7 +172,7 @@ app.use(express.static(path.join(__dirname, "../client", "/webapp/app")))
 
 //Serves the index.html page
 app.get('/', function (req, res) {
-  
+
   res.sendFile(path.join(__dirname, "../client", "/webapp/index.html"))
 })
 
