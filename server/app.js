2a1ad320261e9d12f05e16c7b4f89ad5d74dbb2c
lordbordem <lordbordem@gmail.com>
2017-03-16 15:57:40 +1100

    Waspmote Sensor List
    Added the Waspmote Sensors

--- server/app.js
+++ server/app.js
@@ -5,10 +5,12 @@ var app = express()
   , io = require('socket.io')(server) // new
   , events = require('events')
   , EventEmitter = require('events').EventEmitter
-  , ejs= require('ejs')
+  , ejs= require('ejs');;;;
 
 var path = require('path')
 var moment = require('moment');
+
+var async = require('async');
 
 var underscore = require("underscore.string");
 var math = require('mathjs');
@@ -16,6 +18,52 @@ var math = require('mathjs');
 var sqlite3 = require('sqlite3').verbose()
 ,   db = new sqlite3.Database('RCLSequelize.db')
 ,   Sequelize = require('sequelize');;
+
+var waspmoteList = ['ES_B_04_415_7BD1','ES_B_05_416_7C15','ES_B_10_427_7B90','ES_B_07_420_7E1D','ES_B_09_425_3E8D','ES_B_10_426_7E1E','ES_B_05_417_7C13','ES_B_01_411_7E39','ES_A_05_175_CE2E','ES_B_09_424_B74C','ES_B_12_431_7BC2','ES_B_04_414_7C00','ES_B_07_421_7C17','ES_B_11_429_3E90','ES_B_08_423_7BE2','ES_B_12_430_44DE','ES_B_02_412_3E68','ES_B_06_418_7BED','ES_C_13_302_C88E','ES_B_06_419_7C09','ES_B_08_422_7BDC','ES_B_11_428_3EA4','ES_A_03_141_CE3A','ES_A_03_151_CE38','ES_A_07_293_3E72','ES_A_03_153_CE2A','ES_A_04_163_7C58','ES_A_06_195_3E87','ES_A_13_276_7C44','ES_A_04_159_7B7E','ES_A_09_298_7BC5','ES_A_12_301_7BB6','ES_A_09_226_44DF','ES_A_07_207_44FA','ES_A_06_291_CE30','ES_A_05_181_7BDA','ES_A_11_245_CE2B','ES_A_11_300_7BCD','ES_A_10_234_3E9E','ES_A_06_292_7BBF','ES_A_07_294_7BBC']
+var dbWaspmote = {
+	"ES_B_04_415_7BD1": {"capacity": 21},
+	"ES_B_05_416_7C15": {"capacity": 21},
+	"ES_B_10_427_7B90": {"capacity": 21},
+	"ES_B_07_420_7E1D": {"capacity": 21},
+	"ES_B_09_425_3E8D": {"capacity": 21},
+	"ES_B_10_426_7E1E": {"capacity": 21},
+	"ES_B_05_417_7C13": {"capacity": 21},
+	"ES_B_01_411_7E39": {"capacity": 21},
+	"ES_A_05_175_CE2E": {"capacity": 21},
+	"ES_B_09_424_B74C": {"capacity": 21},
+	"ES_B_12_431_7BC2": {"capacity": 21},
+	"ES_B_04_414_7C00": {"capacity": 21},
+	"ES_B_07_421_7C17": {"capacity": 21},
+	"ES_B_11_429_3E90": {"capacity": 21},
+	"ES_B_08_423_7BE2": {"capacity": 21},
+	"ES_B_12_430_44DE": {"capacity": 21},
+	"ES_B_02_412_3E68": {"capacity": 21},
+	"ES_B_06_418_7BED": {"capacity": 21},
+	"ES_C_13_302_C88E": {"capacity": 21},
+	"ES_B_06_419_7C09": {"capacity": 21},
+	"ES_B_08_422_7BDC": {"capacity": 21},
+	"ES_B_11_428_3EA4": {"capacity": 21},
+	"ES_A_03_141_CE3A": {"capacity": 21},
+	"ES_A_03_151_CE38": {"capacity": 21},
+	"ES_A_07_293_3E72": {"capacity": 21},
+	"ES_A_03_153_CE2A": {"capacity": 21},
+	"ES_A_04_163_7C58": {"capacity": 21},
+	"ES_A_06_195_3E87": {"capacity": 21},
+	"ES_A_13_276_7C44": {"capacity": 21},
+	"ES_A_04_159_7B7E": {"capacity": 21},
+	"ES_A_09_298_7BC5": {"capacity": 21},
+	"ES_A_12_301_7BB6": {"capacity": 21},
+	"ES_A_09_226_44DF": {"capacity": 21},
+	"ES_A_07_207_44FA": {"capacity": 21},
+	"ES_A_06_291_CE30": {"capacity": 21},
+	"ES_A_05_181_7BDA": {"capacity": 21},
+	"ES_A_11_245_CE2B": {"capacity": 21},
+	"ES_A_11_300_7BCD": {"capacity": 21},
+	"ES_A_10_234_3E9E": {"capacity": 21},
+	"ES_A_06_292_7BBF": {"capacity": 21},
+	"ES_A_07_294_7BBC": {"capacity": 21}
+	
+};
 
 var roomsList = ['E1-04201', 'E1-05204', 'E1-05300', 'E1-053L0', 'E1-053V0', 'E1-05402', 'E1-06101', 'E1-06102', 'E1-06103', 'E1-061V2', 'E1-07403', 'E1-07404', 'E1-07405', 'E1-07406', 'E1-08409','E1-09405', 'E1-10104', 'E1-11300', 'E1-11302', 'E1-11403', 'E1-B1100', 'E1-B1102', 'E1-B1103', 'E1-B11V2', 'E1-B11V3', 'E1-B1203', 'E1-B1204', 'E1-B1400', 'E1-B1401', 'E1-B1402', 'E1-B1403', 'E1-B14V0', 'E1-B14V1', 'E1-B14V2', 'E1-B14V3'];
 var dbRooms = 
@@ -200,6 +248,12 @@ var getTimeHalfHourAgo = function(){
 //Calls to function
 getCurrPcLogin(dbRooms, roomsList);
 
+var serverIP = ip.address() ;
+setTimeout(function(){ 
+  server.listen(app.get('port'), serverIP, function(){ 
+  console.log('Express server listening on: ', serverIP, ':', app.get('port'));
+});
+}, 2000);
 
 
 
@@ -228,3 +282,256 @@ app.get('/data', function (req, res) {
 app.listen(3000, function () {
   console.log('Applistening on port 3000!')
 })
+
+//Sockets (Real Time Updating)
+
+io.on('connection', function (socket) {
+    user_count++;
+    socket.emit('users', { number: user_count });
+    socket.broadcast.emit('users', { number: user_count });
+    console.log("user_count: ", user_count);
+    console.log("socket is connected");
+    console.log("socket is connected to user: " + socket.request.user.username);
+    initialpageUpdate();
+    pageUpdate();
+
+    socket.on('disconnect', function () {
+        user_count--;
+        socket.broadcast.emit('users', { number: user_count });
+        console.log(user_count);
+    });
+
+
+    function initialpageUpdate() {
+        setTimeout(function () {
+            console.log("Starting initial page update...");
+            initialUpdate();
+        }, 10000)
+    }
+
+    function pageUpdate() {
+        setTimeout(function () {
+            console.log("Starting page update...");
+            intervalUpdate()
+        }, 25000)
+    }
+
+    // Send time every 2 seconds
+    setInterval(function () {
+        var now = new Date();
+        socket.emit('timer_tick', { string: getTimeStampLog() });
+    }, 1000);
+
+    function intervalUpdate() {
+        setInterval(function () {
+            console.log("Updating Page...");
+            UpdatePage();
+        }, 35000);
+    }
+
+
+    function initialUpdate() {
+        model.temperature.findAll({
+            attributes: ['temp', 'time'],
+            where: {
+                usernameID: socket.request.user.username
+            }
+        }).then(function (data) {
+            //console.log("the temperature variable has: " + JSON.stringify(data));
+            socket.emit('TempJSON', JSON.stringify(data));
+        });
+
+        model.humidity.findAll({
+            attributes: ['h20', 'time'],
+            where: {
+                usernameID: socket.request.user.username
+            }
+        }).then(function (data) {
+            //console.log("the humidity variable has: " + JSON.stringify(data));
+            socket.emit('HumJSON', JSON.stringify(data));
+        });
+
+        model.Monoxide.findAll({
+            attributes: ['CO', 'time'],
+            where: {
+                usernameID: socket.request.user.username
+            }
+        }).then(function (data) {
+            //console.log("the Monoxide variable has: " + JSON.stringify(data));
+            socket.emit('MonoJSON', JSON.stringify(data));
+        });
+        model.Methane.findAll({
+            attributes: ['meth', 'time'],
+            where: {
+                usernameID: socket.request.user.username
+            }
+        }).then(function (data) {
+            //console.log("the Methane variable has: " + JSON.stringify(data));
+            socket.emit('MethJSON', JSON.stringify(data));
+        });
+    };
+
+    var energyPIE;
+    var waterPIE;
+    var gasPIE;
+    var methanePIE;
+    var monoxidePIE;
+
+    function UpdatePage() {
+        model.temperature.findAll({
+            attributes: ['temp'],
+            where: {
+                usernameID: socket.request.user.username
+            }
+        }).then(function (data) {
+
+            var tempArray = [];
+
+            tempArray = data.map(function (data) {
+                return data.temp
+            });
+
+            var averageTemp = math.mean(tempArray);
+            var totalEnergy = 0;
+            if (averageTemp > 25.3) {
+                var totalEnergy = (((averageTemp / 35.3)) - 1);
+            } else {
+                var totalEnergy = (averageTemp / 35.3)
+            }
+            energyPIE = averageTemp;
+            console.log("the energyPIE1 packet has: " + energyPIE);
+            console.log("the total energy consumpution variable has: " + totalEnergy);
+            socket.emit('energyCircle', totalEnergy);
+        });
+
+        model.humidity.findAll({
+            attributes: ['h20'],
+            where: {
+                usernameID: socket.request.user.username
+            }
+        }).then(function (data) {
+
+            var tempArray = [];
+
+            tempArray = data.map(function (data) {
+                return data.h20
+            });
+
+            var averageHumidity = math.mean(tempArray);
+            var totalWater = 0;
+            if (averageHumidity > 55) {
+                var totalWater = (((averageHumidity / 55)) - 1);
+            } else {
+                var totalWater = (averageHumidity / 55);
+            }
+            waterPIE = averageHumidity;
+            console.log("the waterPIE1 packet has: " + waterPIE);
+            console.log("the total water consumpution variable has: " + totalWater);
+            socket.emit('waterCircle', totalWater);
+        });
+        model.Monoxide.findAll({
+            attributes: ['CO'],
+            where: {
+                usernameID: socket.request.user.username
+            }
+        }).then(function (data) {
+            var tempArray = [];
+
+            tempArray = data.map(function (data) {
+                return data.CO
+            });
+
+            var averageMonoxide = math.mean(tempArray);
+
+            monoxidePIE = (averageMonoxide);
+            console.log("the monoxidePIE packet has: " + monoxidePIE);
+        });
+
+        model.Methane.findAll({
+            attributes: ['meth'],
+            where: {
+                usernameID: socket.request.user.username
+            }
+        }).then(function (data) {
+            var tempArray = [];
+
+            tempArray = data.map(function (data) {
+                return data.meth
+            });
+
+            var averageMethane = math.mean(tempArray);
+            methanePIE = (averageMethane);
+            console.log("the methanePie packet has: " + methanePIE);
+        });
+
+
+
+        model.temperature.max('temp', {
+            where: {
+                usernameID: socket.request.user.username
+            }
+
+        }).then(function (data) {
+            console.log("the highest temperature is: " + data);
+            socket.emit('maxTemp', underscore.toNumber(data));
+        });
+        model.humidity.max('h20', {
+            where: {
+                usernameID: socket.request.user.username
+            }
+
+        }).then(function (data) {
+            console.log("the highest humidity is: " + data);
+            socket.emit('maxHumidity', underscore.toNumber(data));
+        });
+        model.Monoxide.max('CO', {
+            where: {
+                usernameID: socket.request.user.username
+            }
+
+        }).then(function (data) {
+            console.log("the highest monoxide is: " + data);
+            socket.emit('maxMono', underscore.toNumber(data) * 10);
+        });
+
+        model.Methane.max('meth', {
+            where: {
+                usernameID: socket.request.user.username
+            }
+
+        }).then(function (data) {
+            console.log("the highest Methane is: " + data);
+            socket.emit('maxMeth', underscore.toNumber(data) * 10);
+        });
+
+        PIEUpdate();
+    }
+
+    function PIEUpdate() {
+        gasPIE = monoxidePIE + methanePIE;
+        console.log("the monoxidePIE packet has: " + gasPIE);
+        console.log("the methanePIE packet has: " + waterPIE);
+        var total = gasPIE + waterPIE + energyPIE;
+        console.log("the gasPIE packet has: " + gasPIE);
+        console.log("the waterPIE packet has: " + waterPIE);
+        console.log("the energyPIE packet has: " + energyPIE);
+        console.log("the total packet has: " + total);
+
+
+        var waterPer = ((waterPIE / total) * 100);
+        var energyPer = (energyPIE / total) * 100;
+        var gasPer = (gasPIE / total) * 100;
+
+        console.log("the waterPer packet has: " + waterPer);
+        console.log("the energyPer packet has: " + energyPer);
+        console.log("the gasPer packet has: " + gasPer);
+
+        var piePacket = [underscore.toNumber(waterPer), underscore.toNumber(energyPer), underscore.toNumber(gasPer)];
+        console.log("the pie packet has: " + piePacket);
+        socket.emit('pieUpdate', piePacket);
+
+    }
+
+
+});
+
